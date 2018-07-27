1769d187db0bc98dcce51d6c43098d97880c013a
garretteklof <garrett.eklof@gmail.com>
2018-07-27 12:46:05 -0500

    mount a 'hello world' Maps component which will soon be tore down

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,43 @@
 import React from "react";
 
-const Home = () => <div>Hello Homies!</div>;
-export default Home;
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.mapContainer = React.createRef();
+  }
+  componentDidMount() {
+    if (!navigator.geolocation) {
+      return alert("Geolocation not supported by your browser :(");
+    }
+    new google.maps.Map(this.mapContainer.current, {
+      center: { lat: 0, lng: 0 },
+      zoom: 1
+    });
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        const geoCoordinates = {
+          lat: coords.latitude,
+          lng: coords.longitude
+        };
+        const map = new google.maps.Map(this.mapContainer.current, {
+          center: geoCoordinates,
+          zoom: 15
+        });
+        new google.maps.Marker({
+          map,
+          position: geoCoordinates,
+          title: "Hello World!"
+        });
+      },
+      () => console.log("Tough beans!")
+    );
+  }
+  render() {
+    return (
+      <div
+        ref={this.mapContainer}
+        style={{ height: "400px", width: "400px" }}
+      />
+    );
+  }
+}

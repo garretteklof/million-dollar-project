a08f4cbd79ecc3b2aed63543198ce10928aa4039
garretteklof <garrett.eklof@gmail.com>
2018-07-30 11:24:22 -0500

    add lifecycle hooks into GoogleMapsWrap && display 10 random markers everytime pan map bounds

--- src/components/UserMap/GoogleMapsWrap.js
+++ src/components/UserMap/GoogleMapsWrap.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { compose, withProps } from "recompose";
+import { compose, withProps, lifecycle } from "recompose";
 import {
   withScriptjs,
   withGoogleMap,
@@ -16,11 +16,62 @@ const GoogleMapsWrap = compose(
     containerElement: <div style={{ height: `400px`, width: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {};
+      this.setState({
+        bounds: null,
+        markers: [],
+        onMapMounted: ref => {
+          refs.map = ref;
+        },
+        onBoundsChanged: () => {
+          this.setState({
+            bounds: refs.map.getBounds(),
+            markers: []
+          });
+        },
+        onIdle: () => {
+          const { bounds, markers } = this.state;
+          if (bounds && !markers.length) {
+            const southWest = bounds.getSouthWest();
+            const northEast = bounds.getNorthEast();
+            const lngSpan = northEast.lng() - southWest.lng();
+            const latSpan = northEast.lat() - southWest.lat();
+            let newMarkerArray = [];
+            for (let i = 0; i < 10; i++) {
+              const position = new google.maps.LatLng(
+                southWest.lat() + latSpan * Math.random(),
+                southWest.lng() + lngSpan * Math.random()
+              );
+              newMarkerArray.push({ position });
+            }
+            this.setState({
+              markers: newMarkerArray
+            });
+          }
+        }
+      });
+    }
+  }),
   withScriptjs,
   withGoogleMap
-)(({ lat, lng, zoom }) => (
-  <GoogleMap center={{ lat, lng }} zoom={zoom}>
-    <Marker position={{ lat, lng }} title="Hello World!" />
+)(props => (
+  <GoogleMap
+    ref={props.onMapMounted}
+    defaultZoom={15}
+    defaultCenter={{ lat: props.lat, lng: props.lng }}
+    onBoundsChanged={props.onBoundsChanged}
+    onIdle={props.onIdle}
+  >
+    <Marker
+      key="ME!"
+      position={{ lat: props.lat, lng: props.lng }}
+      title="IT'S ME, MARIO!"
+    />
+    {props.markers.map((marker, index) => (
+      <Marker key={index} position={marker.position} />
+    ))}
   </GoogleMap>
 ));
 export default GoogleMapsWrap;
--- src/components/UserMap/index.js
+++ src/components/UserMap/index.js
@@ -6,30 +6,35 @@ import LocatorToggle from "./LocatorToggle";
 export default class UserMap extends React.Component {
   state = { lat: 0, lng: 0, showLocation: false };
 
-  onLocatorClick = () => {
-    if (!this.state.showLocation) {
-      navigator.geolocation.getCurrentPosition(
-        ({ coords }) =>
-          this.setState({
-            lat: coords.latitude,
-            lng: coords.longitude,
-            showLocation: true
-          }),
-        () => console.log("ERROR HANDLE HERE!")
-      );
-    } else {
-      this.setState({ showLocation: false });
-    }
+  getCurrentLocation = callback => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) =>
+        this.setState(
+          { lat: coords.latitude, lng: coords.longitude },
+          () => typeof callback === "function" && callback() // optional callback
+        ),
+      () => console.log("ERROR HANDLE HERE!")
+    );
   };
+
+  toggleLocation = () =>
+    this.setState({
+      showLocation: !this.state.showLocation
+    });
+
+  onLocatorClick = () => {
+    this.state.showLocation
+      ? this.toggleLocation()
+      : this.getCurrentLocation(this.toggleLocation);
+  };
+
   overlayMapDisplays = () => {
     if (!navigator.geolocation) {
       return <p>Geolocation not supported by your browser :(</p>;
     } else if (!this.state.showLocation) {
       return <p>Currently Not Showing Map!</p>;
     } else {
-      return (
-        <GoogleMapsWrap lat={this.state.lat} lng={this.state.lng} zoom={15} />
-      );
+      return <GoogleMapsWrap lat={this.state.lat} lng={this.state.lng} />;
     }
   };
 

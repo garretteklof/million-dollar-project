63b3183b95a0da4f4155c46a3d7f39ef5d923f9f
garretteklof <garrett.eklof@gmail.com>
2018-08-19 16:10:06 -0500

    add shared Variables & add MAP_PADDING to keep map height constrained

--- src/components/independent/GoogleMap/Google/google-maps-wrap-props.js
+++ src/components/independent/GoogleMap/Google/google-maps-wrap-props.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 import { GOOGLE_MAPS_API_KEY } from "../../../../config";
+import { MAP_PADDING } from "../../../shared/Variables";
 
 export default {
   googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}`,
   loadingElement: <div style={{ height: `100%` }} />,
-  containerElement: <div style={{ height: `100%`, width: `100%`, position: "relative" }} />,
+  containerElement: (
+    <div
+      style={{
+        maxHeight: `calc(100vh - 2 * ${MAP_PADDING})`,
+        height: `100%`,
+        width: `100%`,
+        position: "relative"
+      }}
+    />
+  ),
   mapElement: <div style={{ height: `100%` }} />
 };
--- src/components/pages/Discovery/index.js
+++ src/components/pages/Discovery/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
+import { MAP_PADDING } from "../../shared/Variables";
+
 import GoogleMap from "../../independent/GoogleMap/";
 import Rolodex from "../../independent/Rolodex";
 
 const Container = styled.div`
-  padding: 2rem;
+  padding: ${MAP_PADDING};
   width: 100%;
   min-height: 100vh;
   display: grid;
